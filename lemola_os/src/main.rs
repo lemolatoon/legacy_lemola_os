627bebc473f05e7b12703ce18088b63c05a7f8c9
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-11-23 17:06:44 +0900

    define target, make libraries compile when build, set intrinsics func(about mem)

--- lemola_os/src/main.rs
+++ lemola_os/src/main.rs
@@ -3,15 +3,20 @@
 
 use core::panic::PanicInfo;
 
+
+// #[no_mangle]  関数の名前を暗号化するのを防ぐ
+// extern "C"  Cの呼び出し規則を使用する
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named '_start' by default
+    loop{}
+}
+
+
 // This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
-// #[no_mangle]  関数の名前を暗号化するのを防ぐ
-// extern "C"  Cの呼び出し規則を使用する
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop{}
-}

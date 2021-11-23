6e8156280d0f3faed2b7d0bfb9a93e60c6f41138
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2021-11-24 02:28:46 +0900

    Add Printer struct

--- lemola_os/src/main.rs
+++ lemola_os/src/main.rs
@@ -21,25 +21,90 @@ pub extern "C" fn _start() -> ! {
             // raw pointer
             *vga_buffer.offset(i as isize * 2) = byte;
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            
         }
     }
 
-    print(MESSAGE, HELLO.len() + 2);
+    let mut printer = Printer::new();
+
+    printer.str_counter += HELLO.len();
+
+    printer.print(b"   ");
+    printer.print_byte(num2byte(0));
+    printer.print_byte(num2byte(3));
+    printer.print_byte(num2byte(2));
+    printer.print_byte(num2byte(1));
+    printer.print(b"   ");
+
+    printer.print(MESSAGE);
+
+    printer.print(b"\nThis is typed from Printer");
+
+
+
+
 
     loop{}
 }
 
-fn print(str: &[u8], written: usize) {
-    let vga_buffer = 0xb8000 as *mut u8;
+struct Printer {
+    str_counter: usize,
+}
 
-    for (i, &byte) in str.iter().enumerate() {
+impl Printer {
+    fn new() -> Printer {
+        Self {str_counter: 0}
+    }
+
+    fn print(&mut self, str: &[u8]) {
+        let vga_buffer = 0xb8000 as *mut u8;
+
+        for (i, &byte) in str.iter().enumerate() {
+            unsafe {
+                // raw pointer
+                *vga_buffer.offset((self.str_counter + i) as isize * 2) = byte;
+                *vga_buffer.offset((self.str_counter + i) as isize * 2 + 1) = 0xb;
+                // self.print_byte(byte);
+            }
+        }
+        self.str_counter += str.len();
+    }
+
+    fn print_byte(&mut self, byte: u8) {
+        let vga_buffer = 0xb8000 as *mut u8;
         unsafe {
             // raw pointer
-            *vga_buffer.offset((written + i) as isize * 2) = byte;
-            *vga_buffer.offset((written + i) as isize * 2 + 1) = 0xb;
+                *vga_buffer.offset(self.str_counter as isize * 2) = byte;
+                *vga_buffer.offset(self.str_counter as isize * 2 + 1) = 0xb;
         }
+        self.str_counter += 1;
+    }
+
+    fn panic(msg: &[u8]) -> usize {
+        let mut printer = Self {str_counter: 900};
+        printer.print(msg);
+        return 0;
     }
 }
+
+fn num2byte(num: usize) -> u8 {
+    let result = match num {
+        0 => 48,
+        1 => 49,
+        2 => 50,
+        3 => 51,
+        4 => 52,
+        5 => 53,
+        6 => 54,
+        7 => 55,
+        8 => 56,
+        9 => 57,
+        _ => Printer::panic(b"illegal argument at num2VGA"),
+    };
+
+    result as u8
+}
+
 
 
 // This function is called on panic.
